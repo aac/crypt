81b467ac70c00550daa2e8c1ae0232eff337d99d
Greplin Guest <greplinguest@Greplin-Guests-MacBook-Pro.local>
2011-03-04 13:03:01 -0800

    Initial checkin.  Includes User scaffolding.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crypt::Application.config.secret_token = 'c004a201a129a40449e77eddb2e83386d4ad4bd34094aa1abb5815104a6809f7c045f1268033b0bac2408acc2b7408fe42f7e21c5ed51b80068328f927f4d63b'
