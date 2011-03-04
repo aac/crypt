f3d6f9fb204349577f78725ccd7b060ab6099ffd
Andrew Cove <andrew.cove@gmail.com>
2011-03-04 13:31:48 -0800

    removing default page and pointing to data index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Crypt::Application.routes.draw do
+  resources :data
   resources :users
 
   # The priority is based upon order of creation:
@@ -57,4 +58,6 @@ Crypt::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root :to => "data#index"
 end
