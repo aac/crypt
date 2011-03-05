2636ec643b9da8bcd9bf415e179331519fb51b47
Andrew Cove <andrew.cove@gmail.com>
2011-03-04 19:12:07 -0800

    generates public key from private key - gem didn't like keys i was getting from keygen.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ require 'crypto'
 class User < ActiveRecord::Base
   has_many :values
   attr_accessor :password
+  
+  before_validation do
+    self.public_key = Crypto::Key.new(self.private_key, self.password).public_key
+  end
 
   def encrypt(data)
     public_key = Crypto::Key.new(self.public_key)
